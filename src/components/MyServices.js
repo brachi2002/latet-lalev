6565d7f50982066a2e073e2ca77c70d7c2addf30
Tzofiya <tzofiya1331@gmail.com>
2024-07-23 06:20:07 +0300

    S

--- src/components/MyServices.js
+++ src/components/MyServices.js
@@ -5,21 +5,21 @@ import { useAuthState } from 'react-firebase-hooks/auth';
 import { auth } from '../firebase';
 import { useTranslation } from 'react-i18next'; 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faUtensils, faBriefcase, faChild, faTshirt, faAmbulance, faBaby } from '@fortawesome/free-solid-svg-icons';
+import { faUtensils, faBriefcase, faChild, faTshirt, faAmbulance, faBabyCarriage } from '@fortawesome/free-solid-svg-icons';
 
-const servicesData = [    { 
-    title: 'Child Emotional Support', 
-    description: 'Recognizing the emotional challenges faced by children in new environments, this service subsidizes professional emotional treatments and therapy. It also ensures that children are well-equipped for school by distributing necessary stationery and school supplies. Seasonal clothing distributions are also organized to provide appropriate attire for winter and summer, helping children feel comfortable and confident throughout the year.', 
-    imageUrls: ['https://firebasestorage.googleapis.com/v0/b/project-d56a6.appspot.com/o/images%2FChild.jpg?alt=media&token=b729413f-6418-4294-9d85-7fb9d805df1e'],
-    icon: faChild
-},
+const servicesData = [
+    { 
+        title: 'Child Emotional Support', 
+        description: 'Recognizing the emotional challenges faced by children in new environments, this service subsidizes professional emotional treatments and therapy. It also ensures that children are well-equipped for school by distributing necessary stationery and school supplies. Seasonal clothing distributions are also organized to provide appropriate attire for winter and summer, helping children feel comfortable and confident throughout the year.', 
+        imageUrls: ['https://firebasestorage.googleapis.com/v0/b/project-d56a6.appspot.com/o/images%2FChild.jpg?alt=media&token=b729413f-6418-4294-9d85-7fb9d805df1e'],
+        icon: faChild
+    },
     { 
         title: 'Job & Training Support', 
         description: 'This service aims to help new immigrants integrate into the workforce by providing comprehensive support. It includes assistance in job searching, resume writing, and interview preparation. Additionally, the program offers vocational training to enhance skills relevant to the job market. Ongoing accompaniment is provided to ensure that individuals can successfully adapt to their new work environments and progress in their careers.', 
         imageUrls: ['https://firebasestorage.googleapis.com/v0/b/project-d56a6.appspot.com/o/images%2FJob.jpg?alt=media&token=b729413f-6418-4294-9d85-7fb9d805df1e'],
         icon: faBriefcase
     },
-
     { 
         title: 'Clothing Distribution Center', 
         description: 'This service operates a center that offers both second-hand and new clothes to families in need. The clothing center is stocked with a wide variety of items suitable for all ages and seasons. Families can visit the center to select clothes that fit their needs and preferences. Additionally, the center organizes periodic distributions where clothes are delivered directly to family homes, ensuring access for those who may have mobility issues or lack transportation.', 
@@ -36,7 +36,7 @@ const servicesData = [    {
         title: 'Maternity Assistance', 
         description: 'This service offers comprehensive support to new mothers, including prenatal and postnatal care. It provides access to maternity supplies, nutritional guidance, and parenting resources. Additionally, the program offers emotional support and counseling to help new mothers navigate the challenges of motherhood, ensuring both the mother and baby are healthy and well-cared for.', 
         imageUrls: ['https://firebasestorage.googleapis.com/v0/b/project-d56a6.appspot.com/o/images%2FMaternity.jpg?alt=media&token=b729413f-6418-4294-9d85-7fb9d805df1e'],
-        icon: faBaby
+        icon: faBabyCarriage
     },
     { 
         title: 'Food Baskets & Shopping Cards', 
