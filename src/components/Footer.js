679a249ee0cbdd98b6adfb0b10e33c3e0a8ba69e
Ayelet Yazdi <ayeletya@post.jce.ac.il>
2024-07-24 11:28:51 +0300

    footer update

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,18 +1,21 @@
 import React from 'react';
+import { useTranslation } from 'react-i18next';
 import './footer.css'; // Ensure this path is correct
 
 const Footer = () => {
+    const { t, i18n } = useTranslation(); 
     return (
         <footer className="footer">
             <div className="footer-content">
                 <div className="footer-section">
-                    <h4>לתת ללב</h4>
-                    <p>אימייל: latetlalev70@gmail.com</p>
-                    <p>טלפון: +123 456 7890</p>
+                    <h4>{t('latat_balev')}</h4>
+                    <p>{t('em1')}</p>
+                    <p>{t('phone3')}</p>
                 </div>
             </div>
             <div className="footer-bottom">
-                &copy; {new Date().getFullYear()} כל הזכויות שמורות לעמותת "לתת ללב" ארגון חסד לקהילה הלטינית בישראל (ע"ר)
+                &copy; {new Date().getFullYear()}
+                {t('text3')}
             </div>
         </footer>
     );
